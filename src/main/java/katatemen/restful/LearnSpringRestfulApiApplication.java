e4ecf71cf1d639309255df430f19a4d34eb0f27a
Muhammad Kholil Aziz Ashari <emkahaaa@gmail.com>
2023-07-18 09:47:30 +0700

    RENAME

--- src/main/java/katatemen/restful/LearnSpringRestfulApiApplication.java
+++ src/main/java/katatemen/restful/LearnSpringRestfulApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnSpringResTfulApiApplication {
+public class LearnSpringRestfulApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringResTfulApiApplication.class, args);
+		SpringApplication.run(LearnSpringRestfulApiApplication.class, args);
 	}
 
 }
--- src/test/java/katatemen/restful/LearnSpringRestfulApiApplicationTests.java
+++ src/test/java/katatemen/restful/LearnSpringRestfulApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LearnSpringResTfulApiApplicationTests {
+class LearnSpringRestfulApiApplicationTests {
 
 	@Test
 	void contextLoads() {
