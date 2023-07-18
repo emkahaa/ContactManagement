34ee57cf076be3c1805e64426ef95d283b2ecb6c
Muhammad Kholil Aziz Ashari <emkahaaa@gmail.com>
2023-07-18 09:38:37 +0700

    INIT SPRINGBOOT

---
+++ src/main/java/katatemen/restful/LearnSpringResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package katatemen.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringResTfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/katatemen/restful/LearnSpringResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package katatemen.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
