867495e99858a710802ae1cbe9e105b62429cb9a
Muhammad Kholil Aziz Ashari <emkahaaa@gmail.com>
2023-07-31 09:20:21 +0700

    USER LOGIN + UNIT TEST

---
+++ src/main/java/katatemen/restful/controller/AuthController.java
@@ -0,0 +1,28 @@
+package katatemen.restful.controller;
+
+import katatemen.restful.model.LoginUserRequest;
+import katatemen.restful.model.TokenResponse;
+import katatemen.restful.model.WebResponse;
+import katatemen.restful.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping(
+            path = "/api/auth/login",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<TokenResponse> login(@RequestBody LoginUserRequest request) {
+        TokenResponse tokenResponse = authService.login(request);
+        return WebResponse.<TokenResponse>builder().data(tokenResponse).build();
+    }
+}
